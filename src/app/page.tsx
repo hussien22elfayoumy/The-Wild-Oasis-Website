208f820ebac342e7107284b6c5d74d5f3716e5cd
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-02-01 22:50:49 +0200

    landing page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import bgImg from '../../public/bg.png';
+
 export default function Home() {
-  return <h1>Hello next!</h1>;
+  return (
+    <section className="mt-24">
+      <Image
+        src={bgImg}
+        fill
+        placeholder="blur"
+        quality={80}
+        className="object-cover object-top"
+        alt="Mountains and forests with two cabins"
+        priority
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-primary-50 mb-10 text-8xl font-normal tracking-tight">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 text-primary-800 hover:bg-accent-600 px-8 py-6 text-lg font-semibold transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </section>
+  );
 }
