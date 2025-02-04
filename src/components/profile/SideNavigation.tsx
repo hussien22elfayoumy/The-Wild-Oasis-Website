5e9f038c0a9009a3ac03a991d0b789600ba3d7f0
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-02-04 21:18:30 +0200

    highlight side links in profile navigation

--- src/components/profile/SideNavigation.tsx
+++ src/components/profile/SideNavigation.tsx
@@ -1,35 +1,38 @@
+'use client';
 import SignOutButton from '@/components/profile/SignOutButton';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { HiCalendarDays, HiHome, HiUser } from 'react-icons/hi2';
 
 const navLinks = [
   {
     name: 'Home',
     href: '/account',
-    icon: <HiHome className="text-primary-600 h-5 w-5" />,
+    icon: <HiHome className="h-5 w-5 text-primary-600" />,
   },
   {
     name: 'Reservations',
     href: '/account/reservations',
 
-    icon: <HiCalendarDays className="text-primary-600 h-5 w-5" />,
+    icon: <HiCalendarDays className="h-5 w-5 text-primary-600" />,
   },
   {
     name: 'Guest profile',
     href: '/account/profile',
 
-    icon: <HiUser className="text-primary-600 h-5 w-5" />,
+    icon: <HiUser className="h-5 w-5 text-primary-600" />,
   },
 ];
 
 function SideNavigation() {
+  const pathName = usePathname();
   return (
-    <nav className="border-primary-900 border-r">
+    <nav className="border-r border-primary-900">
       <ul className="flex h-full flex-col gap-2 text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`hover:bg-primary-900 hover:text-primary-100 text-primary-200 flex items-center gap-4 px-5 py-3 font-semibold transition-colors`}
+              className={`flex items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-colors hover:bg-primary-900 hover:text-primary-100 ${pathName === link.href ? 'bg-primary-900 text-primary-100' : ''}`}
               href={link.href}
             >
               {link.icon}
